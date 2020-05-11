87e9c8a8e65f3a25940f1500995ae4d111f0ece2
Dhruvi Patel <58680738+Dpatel2109@users.noreply.github.com>
2020-05-11 11:08:41 -0700

    Delete create_analysis_short_distance.sql

--- create_analysis_short_distance.sql
+++
@@ -1,6 +0,0 @@
-CREATE EXTERNAL TABLE IF NOT EXISTS short_distance (
-day INT, hour INT, trip_count BIGINT
-)
-ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
-STORED AS TEXTFILE LOCATION '/user/dkansar/short_distance/'
-TBLPROPERTIES ('skip.header.line.count'='2');
