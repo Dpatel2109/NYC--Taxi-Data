aa1f399f4b81e3ae6f552a3cd25a501278899112
Dhruvi Patel <58680738+Dpatel2109@users.noreply.github.com>
2020-05-11 11:08:33 -0700

    Delete create_analysis_peakhours.sql

--- create_analysis_peakhours.sql
+++
@@ -1,6 +0,0 @@
-CREATE EXTERNAL TABLE IF NOT EXISTS Drop analysis (
-trip_distance Day INT, hour INT, Trip_count BIGINT
-)
-ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
-STORED AS TEXTFILE LOCATION '/user/dkansar/Drop_analysis/'
-TBLPROPERTIES ('skip.header.line.count'='2');
