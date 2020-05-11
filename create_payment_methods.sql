e5035319c10da784c1c2926e3ba6ee2799b89969
Dhruvi Patel <58680738+Dpatel2109@users.noreply.github.com>
2020-05-11 11:09:12 -0700

    Delete create_payment_methods.sql

--- create_payment_methods.sql
+++
@@ -1,6 +0,0 @@
-CREATE EXTERNAL TABLE IF NOT EXISTS payment (
-method STRING, method_count BIGINT, Tip_count BIGINT
-)
-ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
-STORED AS TEXTFILE LOCATION '/user/dkansar/payment/'
-TBLPROPERTIES ('skip.header.line.count'='2');
